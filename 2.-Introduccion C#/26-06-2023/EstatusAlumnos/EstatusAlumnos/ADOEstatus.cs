3df7e33e7f7fa8b41d9a6879a5044bd39f6c55f9
JuanPas12 <juanptafolla@gmail.com>
2023-06-26 09:49:43 -0600

    Crear la clase ADOEstatus que implemente la interfaz ICRUD usando ADO .Net

---
+++ 2.-Introduccion C#/26-06-2023/EstatusAlumnos/EstatusAlumnos/ADOEstatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+//Acceso a base de datos
+using System.Data.SqlClient;
+using System.Data;
+//Conexión
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//4. Crear la clase ADOEstatus que implemente la interfaz ICRUD usando ADO .Net 
+namespace EstatusAlumnos
+{
+    public class ADOEstatus : ICRUDEstatus
+    {
+        List<Estatus> _lstEstatus = new List<Estatus>();
+
+
+    }
+}
--- 2.-Introduccion C#/26-06-2023/EstatusAlumnos/EstatusAlumnos/ICRUDEstatus.cs
+++ 2.-Introduccion C#/26-06-2023/EstatusAlumnos/EstatusAlumnos/ICRUDEstatus.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//3. Crear una interface  ICRUDEstatus indicada en la figura anexa 
 namespace EstatusAlumnos
 {
     public interface ICRUDEstatus
