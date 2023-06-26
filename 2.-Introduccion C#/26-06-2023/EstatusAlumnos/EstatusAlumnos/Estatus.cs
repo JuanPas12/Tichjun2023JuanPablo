415b81f5e0b4d76cb9bda394f426e519db18afca
JuanPas12 <juanptafolla@gmail.com>
2023-06-26 09:36:40 -0600

    Crear una clase Estatus indicada en la figura anexa

---
+++ 2.-Introduccion C#/26-06-2023/EstatusAlumnos/EstatusAlumnos/Estatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2. Crear una clase Estatus indicada en la figura anexa 
+namespace EstatusAlumnos
+{
+    public class Estatus
+    {
+        public int id {  get; set; }
+        public string clave { get; set; }
+        public string name { get; set; }
+    }
+}
