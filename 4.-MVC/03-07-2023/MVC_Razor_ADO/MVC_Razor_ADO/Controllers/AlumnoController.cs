3af5aff44fdbdcbb14f5379067f9008245016c88
JuanPas12 <juanptafolla@gmail.com>
2023-07-03 16:41:23 -0600

    MVC_Razor_ADO

---
+++ 4.-MVC/03-07-2023/MVC_Razor_ADO/MVC_Razor_ADO/Controllers/AlumnoController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entidades;
+using Datos;
+using Negocio;
+
+namespace MVC_Razor_ADO.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        NAlumno nAlumno = new NAlumno();
+        List<Alumno> lstAlumnos = new List<Alumno>();
+        List<Estado> lstEstados = new List<Estado>();
+        NEstado nEstado = new NEstado();
+        List<EstatusAlumnos> lstEstatus = new List<EstatusAlumnos>();
+        NEstatusAlumnos nEstatus = new NEstatusAlumnos();
+        
+        public ActionResult Index()
+        {
+            lstAlumnos = nAlumno.Consultar();
+            ViewBag.Estado = lstEstados;
+            ViewBag.Estatus = lstEstatus;
+            return View(lstAlumnos);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Alumno alm = nAlumno.Consultar(id);
+            ViewBag.Estado = lstEstados;
+            ViewBag.Estatus = lstEstatus;
+            return View(alm);
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Alumno alm = nAlumno.Consultar(id);
+            ViewBag.Estado = lstEstados;
+            ViewBag.Estatus = lstEstatus;
+            return View(alm);
+        }
+
+        public ActionResult Create()
+        {
+            lstEstados = nEstado.Consultar();
+            lstEstatus = nEstatus.Consultar();
+            ViewBag.Estado = lstEstados;
+            ViewBag.Estatus = lstEstatus;
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Alumno alm = nAlumno.Consultar(id);
+            lstEstados = nEstado.Consultar();
+            lstEstatus = nEstatus.Consultar();
+            ViewBag.Estado = lstEstados;
+            ViewBag.Estatus = lstEstatus;
+            return View(alm);
+        }
+
+        //-------------------------------------------------------------------
+        [HttpPost]
+        public ActionResult Delete(Alumno alm)
+        {
+            nAlumno.Eliminar(alm.id);
+            
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Create(Alumno alm)
+        {
+            nAlumno.Agregar(alm);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Edit(Alumno alm)
+        {
+            nAlumno.Actualizar(alm);
+            return RedirectToAction("Index");
+        }
+    }
+}
